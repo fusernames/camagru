b80efbae6364ac0874c8fe9ffc0f762aa8dff569
Alexis CAROFF <alcaroff@e1r9p17.42.fr>
2018-12-22 18:57:15 +0100

    oui

--- Controller/PictureController.php
+++ Controller/PictureController.php
@@ -5,11 +5,15 @@ Class PictureController extends AbstractController
 	public function take() {
 		Security::AccessUserOnly();
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			if ($_POST['img'] && $_POST['user_img']) {
-				$dst = imagecreatefrompng($_POST['user_img']);
-				$src = imagecreatefromjpeg(DIR_IMG.$_POST['img']);
-				imagecopymerge($dst, $src);
-				imagepng($dst, DIR_IMG.'a.png');
+			$pic = new Picture();
+			$pic->path = $_FILES['file']['tmp_name'];
+			if (!PictureManager::checkPicture)
+				if ($_POST['img'] && $_POST['user_img']) {
+					$dst = imagecreatefrompng($_POST['user_img']);
+					$src = imagecreatefromjpeg($pic->path);
+					imagecopymerge($dst, $src);
+					imagepng($dst, DIR_IMG.'a.png');
+				}
 			}
 		}
 		return $this->render('picture/take.php');
